43d3b42ee5400b161f2dab0c9a3e423fcfcf378e
RomanVK <roman.kuzmin3333@gmail.com>
2016-07-24 00:27:38 +0300

    Remade UserValidator, UserValidatorTest. Rename ValidUser.json to schema.json and remade its. It remains to add a regular expression for an email and picture in  schema.json

--- src/UserValidator.php
+++ src/UserValidator.php
@@ -1,51 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: r_kuz
- * Date: 22.07.2016
- * Time: 21:39
- */
+
 class UserValidator
 {
-
-    /**
-     * @var string $schema
-     */
-    private $schema = "";
-
     /**
      * @param string $json
      * @return bool
      */
     public function isValidUser(string $json): bool
     {
-        $path_to_schema = __DIR__ . '/../src/ValidUser.json';
-        if(file_exists($path_to_schema)) {
-            $file = fopen($path_to_schema, "r");
-            if ($file) {
-                while(($buffer = fgets($file)) !== false) {
-                    $this->schema = $this->schema.$buffer;
-                    print_r($buffer);
-                }
-                if (!feof($file)) {
-                    return false;//Error: unexpected fgets() fail
-                }
-                fclose($file);
-            }
-        } else {
-          return false; //file is not exist
-        }
-
-        $validator = new \JsonSchema\Validator();
-
-        if (!empty($validator)) {
-            $validator->check($json, $this->schema);
-        } else {
-            throw new Exception();
-        }
-        
-        if (empty($validator->isValid())) return true;
-        
-        return false;
+        $refResolver = new JsonSchema\RefResolver(new JsonSchema\Uri\UriRetriever(), new JsonSchema\Uri\UriResolver());
+        $data = json_decode($json);
+        $schema = $refResolver->resolve('file://'. realpath(__DIR__ . '/../src/schema.json'));
+        $validator = new JsonSchema\Validator();
+        $validator->check($data, $schema);
+        if($validator->isValid())
+            return true;
+        else
+            return false;
     }
 }
--- tests/UserValidatorTest.php
+++ tests/UserValidatorTest.php
@@ -2,21 +2,12 @@
 
 require_once(__DIR__ . '/../src/UserValidator.php');
 
-/**
- * Created by PhpStorm.
- * User: r_kuz
- * Date: 22.07.2016
- * Time: 21:42
- */
 class UserValidatorTest extends PHPUnit_Framework_TestCase
 {
     public  function testIsValidUser()
     {
-        $json = '{"email": "test@example.com", "password": "abc123$%^", "first_name": "John", "last_name": "Doe", "picture": "https://example.com/john.jpg"}';
-
+        $json = file_get_contents(__DIR__ . '/../tests/user_data_example.json');
         $user_validator = new UserValidator();
-
         $this->assertTrue($user_validator->isValidUser($json));
-
     }
 }
