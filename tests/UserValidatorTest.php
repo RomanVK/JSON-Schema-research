ba70cf80bf6234cfb32dccb93e8e7afe4aa2ca5b
RomanVK <roman.kuzmin3333@gmail.com>
2016-07-25 23:54:02 +0300

    remade UserValidatorTest: used @dataProvider for all test functions (json files without)

--- tests/UserValidatorTest.php
+++ tests/UserValidatorTest.php
@@ -14,15 +14,62 @@ class UserValidatorTest extends PHPUnit_Framework_TestCase
         $this->user_validator = new UserValidator();
     }
 
-    public  function testIsValidUserReturnTrue()
+    /**
+     * @return array
+     */
+    public function userValidData()
     {
-        $json = file_get_contents(__DIR__ . '/../tests/user_data_valid.json');
+        return [
+            ['{
+                "email": "email_example@site-name.domain-name",
+                "password": "abc123$%^",
+                "first_name": "John",
+                "last_name": "Doe",
+                "picture": "some-protocol://site_example.com/file_name.some_extension"
+            }']
+        ];
+    }
+
+    /**
+     * @param string $user_valid_data
+     * @dataProvider userValidData
+     */
+    public  function testIsValidUserReturnTrue($user_valid_data)
+    {
+        $json = $user_valid_data;
         $this->assertTrue($this->user_validator->isValidUser($json));
     }
 
-    public  function testIsValidUserReturnFalse()
+    /**
+     * @return array
+     */
+    public function userInvalidData()
     {
-        $json = file_get_contents(__DIR__ . '/../tests/user_data_invalid.json');
+        return [
+            ['{
+                "email": "@site-name.domain-name",
+                "password": "abc123$%^",
+                "first_name": "John",
+                "last_name": "Doe",
+                "picture": "some-protocol://site_example.com/file_name.some_extension"
+            }'],
+            ['{
+                "email": "email_example@site-name.domain-name",
+                "password": "abc123$%^",
+                "first_name": "John",
+                "last_name": "Doe",
+                "picture": "site_example.com/file_name.some_extension"
+            }']
+        ];
+    }
+
+    /**
+     * @param string $user_invalid_data
+     * @dataProvider userInvalidData
+     */
+    public  function testIsValidUserReturnFalse($user_invalid_data)
+    {
+        $json = $user_invalid_data;
         $this->assertFalse($this->user_validator->isValidUser($json));
     }
 }
